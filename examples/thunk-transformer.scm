30b6e4209a0c8ec30b01546e9dc015dc9a9cb9c6
Nick Vanderweit <nick.vanderweit@gmail.com>
2012-08-26 19:57:11 -0600

    Add syntax transformer example
    Transformer turns (thunk ...) into (lambda () ...)

---
+++ examples/thunk-transformer.scm
@@ -0,0 +1,21 @@
+(let ((map ; map higher-order function
+  (let ((mapR
+    (lambda (this f xs)
+      (if (null? xs)
+          '()
+          (cons (f (car xs))
+                (this this f (cdr xs)))))))
+    (mapR mapR))))
+
+  (let ((~thunk ; Define our syntax transformer
+    (lambda (form) ; Takes a single argument: its form
+      (let ((body (car (cdr form)))) ; Second element of form -> body
+           (map ; Use a map to replace ? in following form with body
+             (lambda (id)
+               (if (eq? id '?)
+                   body
+                   id))
+             '(lambda () ?))))))
+
+    ((thunk (+ 1 2))) ; Create a thunk and evaluate it
+  ))
